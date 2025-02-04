01f6adebfbe7dbb0a5cd5f6ead4666799ac55e32
Mihir Patel <47681434+mihirp161@users.noreply.github.com>
2025-02-03 22:33:58 -0500

    Update buddhabrot_rcpp.R
    some optimizations

--- buddhabrot_rcpp.R
+++ buddhabrot_rcpp.R
@@ -66,14 +66,16 @@ buddhabrot <- function(iterations = 1e6, size = 1000, max_iter = 1000) {
     for (res in results) {
       if (is.null(res)) next
       points <- res[[1]]
-      x_coords <- floor(size * (Re(points) + 2) / 4) + 1
-      y_coords <- floor(size * (Im(points) + 2) / 4) + 1
       
-      valid_indices <- which(x_coords > 0 & x_coords <= size & y_coords > 0 & y_coords <= size)
+      x_coords <- floor((Re(points) + 2) * (size - 1) / 4) + 1
+      y_coords_unflipped <- floor((Im(points) + 2) * (size - 1) / 4) + 1
+      y_coords <- size + 1 - y_coords_unflipped  # Flip y-axis
+      
+      valid_indices <- which(x_coords >= 1 & x_coords <= size & y_coords >= 1 & y_coords <= size)
       x_valid <- x_coords[valid_indices]
       y_valid <- y_coords[valid_indices]
       
-      points_matrix[cbind(x_valid, y_valid)] <- points_matrix[cbind(x_valid, y_valid)] + 1
+      points_matrix[cbind(y_valid, x_valid)] <- points_matrix[cbind(y_valid, x_valid)] + 1
     }
     
     gc()  # Explicit garbage collection
@@ -91,4 +93,4 @@ buddhabrot <- function(iterations = 1e6, size = 1000, max_iter = 1000) {
 }
 
 # Run the function with adjusted parameters
-buddhabrot(iterations = 1e6, size = 1000, max_iter = 1500)  # Start with smaller parameters
+buddhabrot(iterations = 1e7, size = 1000, max_iter = 1500)# Start with smaller parameters
